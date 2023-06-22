94c58f025bed4c923f6e3d018546d57ff4730456
ChathumiFernando-code <126304830+ChathumiFernando-code@users.noreply.github.com>
2023-06-23 00:39:17 +0530

    Cards
    First page cards

--- wasana-bakers/src/App.js
+++ wasana-bakers/src/App.js
@@ -1,11 +1,22 @@
 import "./App.css";
+import { BrowserRouter as Router } from 'react-router-dom';
 import Slideshow from "./Components/Slideshow";
 import Footer from "./Components/Footer";
+import CardComponent from "./Components/CardComponent"
+import { styled } from '@mui/system';
+
+const CardContainer = styled('div')({
+  width: '80%', // Set the desired width
+  margin: '0 auto', // Center the div horizontally
+});
 
 function App() {
   return (
     <>
       <Slideshow />
+      <CardContainer>
+        <CardComponent />
+      </CardContainer>
       <Footer />
     </>
   );
